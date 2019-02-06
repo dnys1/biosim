f2ac97689464c3837dd0950092e790e5fe8b42ac
Dillon Nys <dnys1@asu.edu>
2019-02-05 23:59:47 -0600

    Added GENECROP initial config

--- sim/blocks/crop.m
+++ sim/blocks/crop.m
@@ -5,17 +5,53 @@ classdef crop < handle
     properties
         type        % Type of crop being grown (i.e. wheat)
         Ac          % Area of the crop (m2)
+        tPlanted    % The time the crop was planted (used for tracking ET)
+        Tbase       % Base temperature (C)
+        Tcutoff     % Cutoff temperature (C)
+        
+        GDDemer     % GDD required to reach emergence
+        GDDflow     % GDD required to reach flowering stage (C-day)
+        GDDmat      % GDD required to reach maturity
         
         model       % A handle for the model
+        
+        % GENECROP Model variables
+        CPPL
+        CPPP
+        CPR
+        rrmort
+        rrsen
+        DVE
+        
+        POOL        % 
+        LeafB       % Biomass accumulated in leaf (kg)
+        StorB       % Biomass accumulated in storage (kg)
+        StemB       % Biomass accumulated in stems (kg)
+        RootB       % Biomass accumulated in roots (kg)
+        MaxStemB
+        VTIL        % Vegetative tiller shoots
+        REPTIL      % Reproductive tiller shoots
     end
     
     properties (Dependent)
         Kc          % Crop coefficient
         yield       % Yield if harvested now (kg)
         LAI         % Leaf area index
-        Tbase       % Base temperature
-        Tcutoff     % Cutoff temperature
+        Y           % Crop yield (kg)
+        
+        DVS         % Development stage 0 (emergence) - 1 (flowering) - 2 (maturity)
         GDD         % Growing degree days (oC-d)
+        ETTot       % Total evapotranspiration until now (cm)
+        ET          % Evapotranspiration at time t (now)
+        RAD         % Radiation received for current timestep (MJ)
+    end
+    
+    properties (Constant)
+        % GENECROP Model parameters
+        RUE = 1.2       % Radiation use efficiency (g MJ^-1)
+        k = 0.6         % coefficient of light extinction
+        STW = 20        % Dry biomass of one new tiller (g)
+        Maxtil = 900    % Maximum number of tillers
     end
     
     methods
@@ -28,6 +64,50 @@ classdef crop < handle
                 obj.type = type;
                 obj.Ac = Ac;
                 obj.model = model;
+                obj.tPlanted = model.date;
+                
+                switch(obj.type)
+                    case 'Wheat'
+                        obj.Tbase = 0;
+                        obj.Tcutoff = 30;
+                        obj.GDDemer = 120;
+                        obj.GDDflow = 1075;
+                        obj.GDDmat = 1825;
+                    case 'Corn'
+                        obj.Tbase = 10;
+                        obj.Tcutoff = 30;
+                        %obj.GDDemer = 10 * rand + 55;
+                        obj.GDDemer = 60;
+                        obj.GDDflow = 1400;
+                        obj.GDDmat = 2700;
+                    case 'Soy'
+                        obj.Tbase = 10;
+                        obj.Tcutoff = 30;
+                        % emer -http://www.coolbean.info/pdf/soybean_research/early_season/Predicting_soy_emergence.pdf
+                        obj.GDDemer = 72;
+                        % flow,mat =https://edisciplinas.usp.br/pluginfile.php/4111255/mod_resource/content/1/EC%233_2017_Souza2013.pdf
+                        obj.GDDflow = 642;
+                        obj.GDDmat = 1741;
+                end
+                
+                % Load GENECROP model parameters
+                S = load('data/cropParams');
+                obj.CPPL = S.CPPL;
+                obj.CPPP = S.CPPP;
+                obj.CPR = S.CPR;
+                obj.rrmort = S.rrmort;
+                obj.rrsen = S.rrsen;
+                obj.DVE = S.DVE;
+                
+                % Initialize remaining model parameters
+                obj.POOL = 0;
+                obj.LeafB = 0;
+                obj.StorB = 0;
+                obj.StemB = 0;
+                obj.RootB = 0;
+                obj.MaxStemB = 6;
+                obj.VTIL = 0;
+                obj.REPTIL = 0;
             else
                 error('Invalid crop type')
             end
@@ -51,31 +131,95 @@ classdef crop < handle
            end
         end
         
-        function Tbase = get.Tbase(obj)
-            switch(obj.type)
-                case 'Wheat'
-                    Tbase = 0;
-                case 'Corn'
-                    Tbase = 10;
-                case 'Soy'
-                    Tbase = 10;
-            end
-        end
-        
-        function Tcutoff = get.Tcutoff(obj)
-            switch(obj.type)
-                case 'Wheat'
-                    Tcutoff = 30;
-                case 'Corn'
-                    Tcutoff = 30;
-                case 'Soy'
-                    Tcutoff = 30;
-            end
-        end
-        
         function LAI = get.LAI(obj)
-            %TODO
-           LAI = obj.GDD; 
+           if obj.DVS == -1
+               LAI = 0;
+               return
+           end
+           SLA = [0 0.037; 1 0.018; 2 0.017];
+           LAI = obj.LeafB * interp1(SLA(:,1),SLA(:,2),obj.DVS); 
+        end
+        
+        function ET = get.ET(obj)
+            ET = obj.Kc * obj.model.ETo;
+        end
+        
+        function DVS = get.DVS(obj)
+            % part of GENECROP model
+            % https://www.apsnet.org/edcenter/advanced/topics/BotanicalEpidemiology/Pages/CropGrowthModeling.aspx
+            if obj.GDD > obj.GDDemer
+                if obj.GDD < obj.GDDflow
+                    DVS = obj.GDD / obj.GDDflow;
+                else
+                    DVS = 1 + ((obj.GDD - obj.GDDflow)/(obj.GDDmat - obj.GDDflow));
+                end
+            else
+                % Set to -1 if crop has not reached emergence
+                DVS = -1;
+            end
+        end
+        
+        function step(obj)
+            % Only continue if plant has emerged and not reached maturity
+            if obj.DVS == -1 || obj.DVS > 2
+                return
+            end
+            
+            dt = 1/obj.model.dt; % Define separate dt var for calculations
+            
+            % Interpolate necessary variables from model parameters
+            CPPLt = interp1(obj.CPPL(:,1),obj.CPPL(:,2),obj.DVS);
+            CPPPt = interp1(obj.CPPP(:,1),obj.CPPP(:,2),obj.DVS);
+            CPRt = interp1(obj.CPR(:,1),obj.CPR(:,2),obj.DVS);
+            rrmortt = interp1(obj.rrmort(:,1),obj.rrmort(:,2),obj.DVS);
+            rrsent = interp1(obj.rrsen(:,1),obj.rrsen(:,2),obj.DVS);
+            DVEt = interp1(obj.DVE(:,1),obj.DVE(:,2),obj.DVS);
+            
+            % Calculate coefficients
+            CPL = CPPLt * (1-CPRt);
+            CPP = CPPPt * (1-CPRt);
+            CPS = (1-CPL-CPP)*(1-CPRt);
+            
+            % Calculate partitioning of energy
+            PartL   = CPL*obj.POOL;
+            PartS   = CPS*obj.POOL;
+            PartSO  = CPP*obj.POOL;
+            PartR   = CPRt*obj.POOL;
+            PartLS  = PartL+PartS;
+            
+            % Calculate the rate of growth based off ET
+            RG = obj.RAD*obj.RUE*(1-exp(-obj.k*obj.LAI));
+            
+            % Caculate the amount of assimilates available for plant growth
+            obj.POOL = obj.POOL + (RG-PartS-PartL-PartSO-PartR)*dt;
+            
+            % Caclulate production and death of tillers
+            Rmrtv = rrmortt*obj.VTIL;       % Rate of mortality of vegetative tillers
+            Rmortr = rrmortt*obj.REPTIL;    % Rate of mortality of reproductive tillers
+            Rmat = rrmat*obj.VTIL;          % Number of tillers shifting from veg -> rep
+            Rtil = PartLS*obj.STW*(1-(obj.VTIL/obj.Maxtil))*DVEt; % Rate of tiller production
+            
+            obj.REPTIL = obj.REPTIL + (Rmat-Rmortr)*dt;
+            obj.VTIL = obj.VTIL + (Rtil-Rmat-Rmrtv)*dt;
+            
+            % Calculate accumulation of biomass in different parts
+            % Stems
+            rmaxstemb = PartLS;
+            obj.MaxStemB = obj.MaxStemB + rmaxstemb*dt;
+            ddist = 0.005*obj.MaxStemB;
+            if obj.DVS > 1
+                RTransloc = ddist;
+            else
+                RTransloc = 0;
+            end
+            obj.StemB = obj.StemB + (PartS-RTransloc)*dt;
+            % Storage
+            obj.StorB = obj.StorB + (PartSO+RTransloc)*dt;
+            % Roots
+            obj.RootB = obj.RootB + PartR*dt;
+            % Leaves
+            RSenL = rrsent*obj.LeafB;
+            obj.LeafB = obj.LeafB + (PartL-RSenL)*dt;
         end
     end
 end
---
+++ sim/cropTest.m
@@ -0,0 +1,106 @@
+load data/cropParams;
+
+POOL = 0;           
+LeafB = 10;         
+MaxStemb = 6;
+REPTIL = 0;
+RootB = 5;
+StemB = 6;
+STEMP = 320;
+StorB = 0;
+VTIL = 250;
+ddist = 0.005*MaxStemb;
+FST = 0.05;
+k = 0.6;            % coefficient of light extinction
+Maxtil = 900;       % maximum number of tillers (tillers m^-2)
+RAD = 17;           % MJ m^-2 d^-1
+RRMAT = 0.3;
+RUE = 1.2;          % Radiation use efficiency (g MJ^-1)
+STW = 20;           % Dry biomass of one new tiller (g)
+TBASE = 8;
+TFLOW = 1500;
+TMAT = 2000;
+TMAX = 30;
+TMIN = 24;
+Totil = VTIL+REPTIL;
+DVS = 0;
+dt = 1;
+
+i=0;
+while DVS < 2
+    i=i+1;
+    CPPLt = interp1(CPPL(:,1),CPPL(:,2),DVS);
+    CPPPt = interp1(CPPP(:,1),CPPP(:,2),DVS);
+    CPRt = interp1(CPR(:,1),CPR(:,2),DVS);
+    rrmortt = interp1(rrmort(:,1),rrmort(:,2),DVS);
+    rrsent = interp1(rrsen(:,1),rrsen(:,2),DVS);
+    DVEt = interp1(DVE(:,1),DVE(:,2),DVS);
+    SLAt = interp1(SLA(:,1),SLA(:,2),DVS);
+    
+    CPL = CPPLt * (1-CPRt);
+    CPP = CPPPt * (1-CPRt);
+    CPS = (1-CPL-CPP)*(1-CPRt);
+    
+    PartL = CPL*POOL;
+    PartS = CPS*POOL;
+    PartSO= CPP*POOL;
+    PartR = CPRt*POOL;
+    PartLS = PartL + PartS;
+    trackPartL(i)=PartL;
+    trackPartS(i)=PartS;
+    trackPartSO(i)=PartSO;
+    trackPartR(i)=PartR;
+    
+    LAI = LeafB*SLAt;
+    trackLAI(i) = LAI;
+    
+    RGrowth = RAD*RUE*(1-exp(-k*LAI));
+    
+    POOL = POOL + (RGrowth-PartS-PartL-PartSO-PartR)*dt;
+    
+    RSenL = rrsent*LeafB;
+    LeafB = LeafB + (PartL-RSenL)*dt;
+    
+    rmaxstemb = PartLS;
+    MaxStemb = MaxStemb + rmaxstemb*dt;
+    ddist = 0.005*MaxStemb;
+    
+    Rmrtv = rrmortt * VTIL;
+    Rmat = RRMAT*VTIL;
+    Rmortr = rrmortt*REPTIL;
+    REPTIL = REPTIL + (Rmat-Rmortr)*dt;
+    
+    RootB = RootB + PartR*dt;
+    
+    if DVS > 1
+        RTransloc = ddist;
+    else
+        RTransloc = 0;
+    end
+    StemB = StemB + (PartS-RTransloc)*dt;
+    
+    Dtemp = ((TMAX+TMIN)/2)-TBASE;
+    STEMP = STEMP + Dtemp*dt;
+    
+    StorB = StorB + (PartSO+RTransloc)*dt;
+    
+    Rtil = PartLS*STW*(1-(VTIL/Maxtil))*DVEt;
+    VTIL = VTIL + (Rtil-Rmat-Rmrtv)*dt;
+    
+    trackRootB(i)=RootB;
+    trackStorB(i)=StorB;
+    trackStemB(i)=StemB;
+    trackLeafB(i)=LeafB;
+    
+    if STEMP < TFLOW
+        DVS = STEMP / TFLOW;
+    else
+        DVS = 1+((STEMP-TFLOW)/(TMAT-TFLOW));
+    end
+end
+
+i=1:1:i;
+plot(i,trackRootB,i,trackStorB,i,trackStemB,i,trackLeafB);
+hold on
+legend('RootB','StorB','StemB','LeafB');
+hold off
