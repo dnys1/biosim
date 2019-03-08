b5a5e2cc2834ad5ce93ba02d9e8d5701d096d4e1
f2.1205989495 <dnys1@sundevils.asu.edu>
2019-03-08 12:33:49 -0700

    Added Green-Ampt eq. for when q>Ks

--- sim/blocks/wetland1.m
+++ sim/blocks/wetland1.m
@@ -11,7 +11,11 @@ classdef wetland1 < handle
     
     properties (Dependent)
         t           % Estimated Green-Ampt HRT (s)
+        f           % Estimated Green-Ampt infiltration rate (m/s)
+        F           % Estimated Green-Ampt wetted front (m)
         psi_f       % Wetting front suction head (m)
+        NH3_0       % Initial un-ionized ammonia (mg/L)
+        NH4_0       % Initial ionized ammonia (mg/L)
     end
     
     properties (Constant)
@@ -33,6 +37,11 @@ classdef wetland1 < handle
         kA_TP =0.03/24  % Areal rate constant for TP  (m/hr)
         d10=6e-3        % d10 diameter (m)
         d60=8.5e-3      % d60 diameter (m)
+        COD_0=600       % Initial COD in wastewater (mg/L)
+        BOD_0=300       % Initial BOD5 in wastewater (mg/L)
+        NH3T_0=25       % Initial NH3T (=[NH3]+[NH4+]) in wastewater (mg/L)
+        TP_0=7          % Initial TP in wastewater (mg/L)
+        pH_0=7.0        % Initial pH in wastewater
     end
     
     methods
@@ -45,24 +54,39 @@ classdef wetland1 < handle
             obj.model = model;
         end
         
-        function [COD, BOD, TSS, NH4, TP] = kinetics(obj, C)
+        function [COD, BOD, TSS, NH4, TP] = kinetics(obj)
             %KINETICS Outputs the concentrations of COD,BOD,TSS,NH4,TP
-            %given input concentrations. C must be a vector of length 5
-            if len(C) ~= 5
-                error('C must be vector of length 5 [COD, BOD, TSS, NH4, TP]');
-            end
-            COD = C(1)*exp(-obj.kV_COD*obj.t);
-            BOD = C(2)*exp(-obj.kV_BOD*obj.t);
-            TSS = C(3)*exp(-obj.kV_TSS*obj.t);
-            NH4 = C(4)*exp(-obj.kV_NH4*obj.t);
-            TP  = C(5)*exp(-obj.kV_TP*obj.t);
+            %given initial concentrations which are assumed unchanging
+            COD = obj.COD_0*exp(-obj.kV_COD*obj.t);
+            BOD = obj.BOD_0*exp(-obj.kV_BOD*obj.t);
+            TSS = obj.TSS_0*exp(-obj.kV_TSS*obj.t);
+            NH4 = obj.NH4_0*exp(-obj.kV_NH4*obj.t);
+            TP  = obj.TP_0*exp(-obj.kV_TP*obj.t);
+        end
+        
+        function NH3_0 = get.NH3_0(obj)
+        % Calculate the ammount of un-ionized ammonia using
+        % Emmerson relationship
+            pK = 0.09018 + 2729.92/obj.model.Ta_K;
+            Fu = 1/(1+(10^-obj.pH_0/10^-pK));
+            NH3_0 = obj.NH3T_0 * Fu;
+        end
+        
+        function NH4_0 = get.NH4_0(obj)
+        % Calculate the ammount of ionized ammonia using
+        % Emmerson relationship
+            NH4_0 = obj.NH3T_0 - obj.NH3_0;
         end
         
         function psi_f = get.psi_f(obj)
+        % Estimate the wetting front suction head using
+        % air-entry suction head
             psi_f = obj.psi_ae / 2;
         end
         
         function t = get.t(obj)
+        % Calculation of hydraulic residence time t using
+        % Green-Ampt modeling for a single-layer wetland
             if obj.q > obj.Ks
                 h0 = obj.q - obj.Ks;
             else
@@ -71,6 +95,45 @@ classdef wetland1 < handle
             h = h0 - obj.psi_f;
             t = (obj.theta_s - obj.theta_r)/obj.Ks * (obj.z - h*log(1 + obj.z/h));
         end
+        
+        function f = get.f(obj)
+        % Calculation of infiltration rate
+            if obj.q < obj.Ks
+                f = @(t)obj.q;
+            else
+                tp = obj.Ks*abs(obj.psi_f)*(obj.phi-obj.theta_r)/(obj.q*(obj.q-obj.Ks));
+                T_ = abs(obj.psi_f)*(obj.phi-obj.theta_r)/obj.Ks;
+                Fp = obj.q * tp;
+                tc = Fp/obj.Ks - (abs(obj.psi_f)*(obj.phi-obj.theta_r))/obj.Ks * ...
+                        log(1+Fp/(abs(obj.psi_f)*(obj.phi-obj.theta_r)));
+                f = @(t)(obj.Ks*(0.707*(((t-tp+tc)+T_)/(t-tp+tc))^0.5 + 0.667 - 0.236*((t-tp+tc)/((t-tp+tc)+T_))^0.5 - ...
+                        0.138 * ((t-tp+tc)/(t-tp+tc)+T_)));
+            end
+        end
+        
+        function Fstep = Fstep(obj, t)
+            Fguess = obj.Ks*t;
+            Fstep = obj.Ks*t + abs(obj.psi_f)*(obj.phi-obj.theta_r)*log(1+Fguess/(abs(obj.psi_f)*(obj.phi-obj.theta_r)));
+            while abs(Fguess-Fstep) > 1e-5
+                Fguess = obj.Ks*t + abs(obj.psi_f)*(obj.phi-obj.theta_r)*log(1+Fstep/(abs(obj.psi_f)*(obj.phi-obj.theta_r)));
+                Fstep = obj.Ks*t + abs(obj.psi_f)*(obj.phi-obj.theta_r)*log(1+Fguess/(abs(obj.psi_f)*(obj.phi-obj.theta_r)));
+            end
+        end
+        
+        function F = get.F(obj)
+        % Calculation of wetting front depth
+            if obj.q < obj.Ks
+                F = @(t)(obj.Fstep(t));
+            else
+                tp = obj.Ks*abs(obj.psi_f)*(obj.phi-obj.theta_r)/(obj.q*(obj.q-obj.Ks));
+                T_ = abs(obj.psi_f)*(obj.phi-obj.theta_r)/obj.Ks;
+                Fp = obj.q * tp;
+                tc = Fp/obj.Ks - (abs(obj.psi_f)*(obj.phi-obj.theta_r))/obj.Ks * ...
+                        log(1+Fp/(abs(obj.psi_f)*(obj.phi-obj.theta_r)));
+                F = @(t)(obj.Ks*(0.529*(t-tp+tc) + 0.471*(T_*(t-tp+tc)+(t-tp+tc)^2)^0.5 + 0.0128*T_* ...
+                        (log((t-tp+tc)+T_)-log(T_)) + 0.471*T_*(log((t-tp+tc)+0.5*T_+(T_*(t-tp+tc)+(t-tp+tc)^2)^0.5) - ...
+                        log(0.5*T_))));
+            end
+        end
     end
 end
-
