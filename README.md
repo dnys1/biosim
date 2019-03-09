c8bf8513a479a1fda1b6d9a8db62e9005581dac5
Dillon Nys <dnys1@asu.edu>
2019-03-09 13:33:29 -0600

    Added documentation for runModel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # biosim
 BioSim code and paper
+
+To run simulation, run `sim/runModel.m` in MATLAB.
--- sim/runModel.m
+++ sim/runModel.m
@@ -4,23 +4,34 @@ addpath eqn;
 
 clc; clearvars;
 
+% BioSim dimensions
 L = 10;
 W = 10;
 H = 10;
+
+% Initial pressure and temperatures
 P = 101.325;
 Ta = 20;
 To = 20;
 RH = 0.5;
+
+% Time step
 dt = time.MINUTE;
 
+% Length of simulation
 hours = 1:1:168*2;
+
+% Build and run the model
 m = model(L, W, H, P, Ta, To, RH, dt);
 out = m.run(hours);
 
+% Plot the output of air and water temperatures
 plot(hours, out.Ta, hours, out.Tw);
 hold on
 legend('Ta', 'Tw');
 hold off
+
+% Plot the output of crop simulation
 figure;
 c=m.cropBlocks(1);
 plot(hours,c.trackLeafB/1000,hours,c.trackStorB/1000,hours,c.trackStemB/1000,hours,c.trackRootB/1000);
